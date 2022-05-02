06b1f9597ad27e7b2dc632e68d650bcd28ae3633
henrque <henrque02@gmail.com>
2022-05-02 09:34:56 -0300

    ENG-28: estrutura inicial do backend

---
+++ src/test/java/com/eng/taxonhub/TaxonhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eng.taxonhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxonhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
