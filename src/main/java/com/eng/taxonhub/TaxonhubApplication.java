2ba369201a42994438c4f00f0367ea891a2d0508
Vinny <vinnyhsz1@gmail.com>
2022-05-15 19:52:29 -0300

    ENG-2: upload de arquivo

--- src/main/java/com/eng/taxonhub/TaxonhubApplication.java
+++ src/main/java/com/eng/taxonhub/TaxonhubApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
 @SpringBootApplication
 @EnableScheduling
 @EnableAutoConfiguration
