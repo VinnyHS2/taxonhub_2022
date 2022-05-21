30a0ede5b8bcf663314e04298d1d3fc761f07fe5
unknown <julianoKS1999@hotmail.com>
2022-05-21 19:46:08 -0300

    ENG-18: Atualizacao GetMapping

--- src/main/java/com/eng/taxonhub/controller/StorageController.java
+++ src/main/java/com/eng/taxonhub/controller/StorageController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +34,7 @@ public class StorageController {
 		return ResponseEntity.ok().build();
 	}
 	
-	@PostMapping("/{validateCSV}")
+	@GetMapping("/{validateCSV}")
 	public ResponseEntity<?> validationCSV(@PathVariable(name = "validateCSV", required = true) String validateCSV) throws Exception{
 		
 		storageService.validarCSV(validateCSV);
